554ba22ce6b3798fbd56572c78462be8d3f0c43f
AbdulAAbdalla <prelegallogistics@gmail.com>
2024-03-18 18:20:51 +0300

    writing code as instructed in the assignment

--- functions.dart
+++ functions.dart
@@ -1,2 +1,9 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+//## Program 2: Perform Mathematical Operations with Functions
+//Write a Dart program that performs two mathematical operations using functions.
+
+void main(){
+var x=10;
+var y=8;
+var c =(x/y)*2;
+print ("The Value of c is $c");
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,9 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//## Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+void main(){
+String name="Ali Abdul";
+int age= 35;
+String school= "Zetech University";
+String hobby="playing football";
+print ("My name is $name,I am $age years old, I study at $school and my fevourite hobby is $hobby.");
+}
